d538d1f4e497fc3308b3b6a3c2567a5f6d5c28e8
Mehul Garg <mehulgarg14@gmail.com>
2019-10-17 12:03:22 +0530

    Login Page Done

--- src/app/learning-platform/learning-platform.module.ts
+++ src/app/learning-platform/learning-platform.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {LearningPlatformRoutingModule, routingComponents} from './learning-platform-routing.module';
+import { LearningPlatformRoutingModule, routingComponents } from './learning-platform-routing.module';
+import { DemoMaterialModule } from '../core/material-module';
 @NgModule({
   declarations: [routingComponents],
   imports: [
     CommonModule,
     LearningPlatformRoutingModule,
+    DemoMaterialModule
   ]
 })
 export class LearningPlatformModule { }
--- src/app/learning-platform/login/login.component.ts
+++ src/app/learning-platform/login/login.component.ts
@@ -10,6 +10,7 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    document.body.classList.add('bg-img');
   }
 
 }
