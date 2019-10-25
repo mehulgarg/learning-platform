84268a41cc311c6834d9710579137178cffa41a0
Onurene <onurene22@gmail.com>
2019-10-25 15:14:17 +0530

    added files

---
+++ src/app/learning-platform/components/available-modules/available-modules.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output, OnInit ,OnDestroy} from '@angular/core';
+import { SelectedModulesService } from '../../services/selected-modules.service';
+
+
+@Component({
+  selector: 'app-available-modules',
+  templateUrl: './available-modules.component.html',
+  styleUrls: ['./available-modules.component.scss']
+
+})
+export class AvailableModulesComponent{
+
+  courses:string[] = ["Laravel","Django","Flask"]
+
+  constructor(private tempuser:SelectedModulesService) { }
+
+  frameworksTaken(e){
+    console.log(e.target.textContent)
+    this.tempuser.user.addCourses(e.target.textContent)
+  }
+
+  ngOnInit() {
+  }
+}
+
